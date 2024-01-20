5701247213656be9098bc226766f3e3d725fadad
GreendeeRoper444 <121622281+GreendeeRoper444@users.noreply.github.com>
2024-01-20 23:46:59 +0800

    changes

--- client/src/Create.jsx
+++ client/src/Create.jsx
@@ -11,7 +11,11 @@ function Create() {
 
   const Submit = (e) =>{
     e.preventDefault();
-    axios.post("http://localhost:3001/createUser", {name, email, age})
+    axios.post("http://localhost:3001/createUser", {
+      name, 
+      email, 
+      age
+    })
     .then(result=> {
       console.log(result)
       navigate('/')
